405b57a49a09823d4603f65ea034e2a6d9916953
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-10 21:59:01 +0100

    Add cmp dependecies

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,22 @@
+-- Neovim plugins
+
+return {
+  -- Base
+  { "wbthomason/packer.nvim" } -- Have packer manage itself
+  { "nvim-lua/popup.nvim" } -- An implementation of the Popup API from vim in Neovim
+	{ "nvim-lua/plenary.nvim" } -- Useful lua functions used ny lots of plugins
+
+  -- Colorschemes
+	{ "aktersnurra/no-clown-fiesta.nvim" } -- My colorscheme
+
+  -- cmp plugins
+  { "hrsh7th/nvim-cmp" } -- The completion plugin
+  { "hrsh7th/cmp-buffer" } -- buffer completions
+  { "hrsh7th/cmp-path" } -- path completions
+  { "hrsh7th/cmp-cmdline" } -- cmdline completions
+  { "saadparwaiz1/cmp_luasnip" } -- snippet completions
+
+  -- Snippets
+  { "L3MON4D3/LuaSnip" } --snippet engine
+  { "rafamadriz/friendly-snippets" } -- a bunch of snippets to use
+}
