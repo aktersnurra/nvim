3995ebcdbd252927840ac4e2a81e881d4a98adce
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-10 22:25:42 +0100

    Add lsp sources to cmp

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -101,6 +101,8 @@ cmp.setup({
 			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
 			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
 			vim_item.menu = ({
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[NVIM_LUA]",
 				luasnip = "[Snippet]",
 				buffer = "[Buffer]",
 				path = "[Path]",
@@ -110,6 +112,8 @@ cmp.setup({
 	},
 	-- TODO: check out more sources
 	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
 		{ name = "luasnip" },
 		{ name = "buffer" },
 		{ name = "path" },
