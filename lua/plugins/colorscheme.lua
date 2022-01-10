b980f3abb417b5b8bd48ee4d007795702e070029
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-10 21:59:25 +0100

    Add colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+-- Neovim colorscheme.
+
+local colorscheme = "default"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+	vim.notify("colorscheme " .. colorscheme .. " not found!")
+	return
+end
