b210e2936ef1233b65386fb27229157562cb7acd
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-11 22:34:55 +0100

    Add treesitter config

---
+++ lua/plugins/config/treesitter.lua
@@ -0,0 +1,19 @@
+-- Treesitter is a tool for building syntax trees for source files.
+-- In the neovim context it helps with better coloring.
+
+local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+	return
+end
+
+treesitter.setup({
+	ensure_installed = "maintained",
+	sync_install = false,
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = true,
+	},
+	indent = { enable = true, disable = { "yaml" } },
+})
