0f24d6d470d5fafdfbfae73cb44acd83a9ff604e
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-11 21:39:53 +0100

    Add sumneko lua config

---
+++ lua/plugins/lsp/settings/sumneko_lua.lua
@@ -0,0 +1,18 @@
+-- Config for a Lua language server.
+
+return {
+	settings = {
+
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+		},
+	},
+}
