7a61f2e8e9fe2b5850204b2e1553f6da08b992a0
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-13 19:21:28 +0100

    Fix paths for lsp installer

--- lua/config/lsp/lsp-installer.lua
+++ lua/config/lsp/lsp-installer.lua
@@ -9,22 +9,22 @@ end
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
 	local opts = {
-		on_attach = require("plugins.lsp.handlers").on_attach,
-		capabilities = require("plugins.lsp.handlers").capabilities,
+		on_attach = require("config.lsp.handlers").on_attach,
+		capabilities = require("config.lsp.handlers").capabilities,
 	}
 
 	if server.name == "jsonls" then
-		local jsonls_opts = require("plugins.lsp.settings.jsonls")
+		local jsonls_opts = require("config.lsp.settings.jsonls")
 		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
 	end
 
 	if server.name == "sumneko_lua" then
-		local sumneko_opts = require("plugins.lsp.settings.sumneko_lua")
+		local sumneko_opts = require("config.lsp.settings.sumneko_lua")
 		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 	end
 
 	if server.name == "pyright" then
-		local pyright_opts = require("plugins.lsp.settings.pyright")
+		local pyright_opts = require("config.lsp.settings.pyright")
 		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
 	end
 
