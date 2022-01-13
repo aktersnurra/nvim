f75ad229a77f75fbf0b8469b98106b2019e0b694
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-13 21:57:49 +0100

    Add zen mode

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -43,4 +43,13 @@ return {
 	{ "goolord/alpha-nvim" },
 	{ "antoinemadec/FixCursorHold.nvim" }, -- This is needed to fix lsp doc highlight
 	{ "folke/which-key.nvim" },
+    { "folke/zen-mode.nvim",
+      config = function()
+        require("zen-mode").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+    end
+    },
 }
