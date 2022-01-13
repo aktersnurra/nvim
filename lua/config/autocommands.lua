9d4d8779af0acab902f0b1c1e26c9c2d9ee32283
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-13 23:25:44 +0100

    Fix format on save

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -26,7 +26,7 @@ vim.cmd([[
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
-  Autoformat
+
   augroup _lsp
     autocmd!
     autocmd BufWritePre * lua vim.lsp.buf.formatting()
