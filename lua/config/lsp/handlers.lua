f9cb5099df7f2eb2cceb715515b2d7a98d6cbc9a
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-13 20:13:34 +0100

    Fix error with lsp handler

--- lua/config/lsp/handlers.lua
+++ lua/config/lsp/handlers.lua
@@ -98,9 +98,8 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
-	return
+	M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 return M
