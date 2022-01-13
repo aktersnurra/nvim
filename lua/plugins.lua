19ad6f8214bcc4c7fe758c3df8dc38e5a48848c6
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-13 20:57:17 +0100

    Add cmp-nvim-lsp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,11 +5,14 @@ return {
 	{ "nvim-lua/popup.nvim" }, -- An implementation of the Popup API from vim in Neovim
 	{ "nvim-lua/plenary.nvim" }, -- Useful lua functions used ny lots of plugins
 	{ "aktersnurra/no-clown-fiesta.nvim" },
+
 	{ "hrsh7th/nvim-cmp" }, -- The completion plugin
 	{ "hrsh7th/cmp-buffer" }, -- buffer completions
 	{ "hrsh7th/cmp-path" }, -- path completions
 	{ "hrsh7th/cmp-cmdline" }, -- cmdline completions
 	{ "saadparwaiz1/cmp_luasnip" }, -- snippet completions
+	{ "hrsh7th/cmp-nvim-lsp" },
+
 	{ "L3MON4D3/LuaSnip" },
 	{ "rafamadriz/friendly-snippets" },
 	{ "neovim/nvim-lspconfig" },
