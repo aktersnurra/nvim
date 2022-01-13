9a09b2c68742d83437acba20d4cafcc2e334b05e
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-13 22:41:38 +0100

    Update whichkey symbol

--- lua/config/whichkey.lua
+++ lua/config/whichkey.lua
@@ -35,7 +35,7 @@ local setup = {
 	},
 	icons = {
 		breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-		separator = "➜", -- symbol used between a key and it's label
+		separator = "", -- symbol used between a key and it's label
 		group = "+", -- symbol prepended to a group
 	},
 	popup_mappings = {
