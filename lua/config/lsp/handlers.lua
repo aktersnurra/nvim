8c8c00b86178d7da08a29f43cd12a1f643710244
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-13 20:57:47 +0100

    Update handler

--- lua/config/lsp/handlers.lua
+++ lua/config/lsp/handlers.lua
@@ -98,8 +98,9 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
-	M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+    return
 end
 
+M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 return M
