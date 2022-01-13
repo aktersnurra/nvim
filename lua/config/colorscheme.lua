ddf461e5b78dc3abefb17cf6d55942c14236297d
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-13 20:56:00 +0100

    Update default colorscheme

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,6 +1,6 @@
 -- Neovim colorscheme.
 
-local colorscheme = "default"
+local colorscheme = "no-clown-fiesta"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
