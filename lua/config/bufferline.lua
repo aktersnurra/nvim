d70376f363acbce81bb3696cfbeab8c009f5a24b
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-14 21:45:55 +0100

    Set icons to false in bufferline

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -56,7 +56,7 @@ bufferline.setup({
 		--   end
 		-- end,
 		offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
-		show_buffer_icons = true,
+		show_buffer_icons = false,
 		show_buffer_close_icons = false,
 		show_close_icon = false,
 		show_tab_indicators = false,
