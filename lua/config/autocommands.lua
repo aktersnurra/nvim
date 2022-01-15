b0d67e5038480a3836a4be59bc0819c30a8ca4de
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-15 21:17:18 +0100

    Remove autoformat on save

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -26,9 +26,4 @@ vim.cmd([[
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
-
-  augroup _lsp
-    autocmd!
-    autocmd BufWritePre * lua vim.lsp.buf.formatting()
-  augroup end
 ]])
