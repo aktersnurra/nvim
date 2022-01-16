473629b4d6e0cbec1c2c6d78f96cef8e85e2e044
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-16 10:39:13 +0100

    Enable s for hop

--- lua/config/hop.lua
+++ lua/config/hop.lua
@@ -4,5 +4,5 @@ if not status_ok then
 end
 
 hop.setup()
--- vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
 vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
