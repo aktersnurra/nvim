52f2c2c67eb962f3d69aa57e3602c29beb643a14
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-18 18:34:43 +0100

    Remove require from load_extension for telescope

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -116,5 +116,5 @@ telescope.setup({
 	},
 })
 
-require("telescope").load_extension("fzf")
-require("telescope").load_extension("file_browser")
+telescope.load_extension("fzf")
+telescope.load_extension("file_browser")
