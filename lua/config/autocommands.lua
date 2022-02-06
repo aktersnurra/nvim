1261093f90ce598e58ebd2c0dc40e1cf9de7cdd5
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-02-06 19:43:26 +0100

    chore: format autocommands

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -8,20 +8,24 @@ vim.cmd [[
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
   augroup end
+
   augroup _git
     autocmd!
     autocmd FileType gitcommit setlocal wrap
     autocmd FileType gitcommit setlocal spell
   augroup end
+
   augroup _markdown
     autocmd!
     autocmd FileType markdown setlocal wrap
     autocmd FileType markdown setlocal spell
   augroup end
+
   augroup _auto_resize
     autocmd!
     autocmd VimResized * tabdo wincmd = 
   augroup end
+
   augroup _alpha
     autocmd!
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
