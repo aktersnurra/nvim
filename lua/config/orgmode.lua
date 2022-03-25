d493c593255906caed49bf66a6b75a3fce057041
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-03-26 00:28:04 +0100

    feat(orgmode): add new templates

--- lua/config/orgmode.lua
+++ lua/config/orgmode.lua
@@ -9,4 +9,12 @@ orgmode.setup_ts_grammar()
 orgmode.setup {
   org_agenda_files = { "~/.local/share/org/**/*" },
   org_default_notes_file = "~/.local/share/org/refile.org",
+  org_agenda_templates = {
+    t = { description = "Task", template = "* TODO %?\n %u" },
+    m = {
+      description = "Meeting",
+      template = "* MEETING %? :MEETING:\n:LOGBOOK:\nCLOCK: %U\n:END:",
+    },
+    n = { description = "Note", template = "* NOTE %? :NOTE:%?\n %u" },
+  },
 }
