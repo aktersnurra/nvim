e1c5a423a6e8abec507d89863903be869762a55e
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-04-06 23:47:53 +0200

    chore(lua): nuke lua files

--- lua/config/colorscheme.lua
+++
@@ -1,9 +0,0 @@
--- Neovim colorscheme.
-
-local colorscheme = "no-clown-fiesta"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
