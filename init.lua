143d0de99399e9d6b4779ebd91a1cbcee5df5dd5
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-04-07 22:46:45 +0200

    feat(bootstrap): convert script to lua

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
 -- Loads plugins for Neovim.
 
+-- Bootstap essential plugins
+require "bootstrap"
+
 -- Load fennel config
 vim.g["aniseed#env"] = {
   module = "init",
   compile = true,
 }
-
--- require "init"
