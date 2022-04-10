912073a2c24f8342b34ccc29ea3480d887b99be5
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-04-11 00:01:55 +0200

    Revert "fix(bootstrap): mv autocmd"
    This reverts commit f3f910938dfc032b7e39412aa09deac35da1984e.

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -3,6 +3,14 @@ if not status_ok then
   return
 end
 
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
 -- Have packer use a popup window
 packer.init {
   display = {
