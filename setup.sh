25aa9cf364775086ff285f1291dc1644fc9920b4
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-04-12 20:26:23 +0200

    feat(setup): add setup shell script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+which nvim >/dev/null 2>&1 || echo "Neovim not installed!" && exit 1
+which luarocks >/dev/null 2>&1 || echo "Luarocks not installed!" && exit 1
+luarocks list | grep "fennel" || echo "fennel is not installed" && exit 1
+
+nvim --headless +qa && nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync"
