5d4055265593cfc5765273d1b692c436f1874e47
aktersnurra <grydholm@kth.se>
2022-04-24 01:35:21 +0200

    style: rename ensure to bootstrap

--- init.lua
+++ init.lua
@@ -4,15 +4,15 @@ local fmt = string.format
 local execute = vim.api.nvim_command
 local packer_path = fn.stdpath "data" .. "/site/pack/packer/start"
 
-local function ensure(user, repository)
-  local ensure_path = fmt("%s/%s", packer_path, repository)
-  if fn.empty(fn.glob(ensure_path)) > 0 then
+local function bootstrap(user, repository)
+  local bootstrap_path = fmt("%s/%s", packer_path, repository)
+  if fn.empty(fn.glob(bootstrap_path)) > 0 then
     execute(
       fmt(
         "!git clone --depth 1 https://github.com/%s/%s %s",
         user,
         repository,
-        ensure_path
+        bootstrap_path
       )
     )
     execute(fmt("packadd %s", repository))
@@ -20,9 +20,9 @@ local function ensure(user, repository)
 end
 
 -- Bootstrap install essential modules if not present
-ensure("wbthomason", "packer.nvim")
-ensure("Olical", "aniseed")
-ensure("lewis6991", "impatient.nvim")
+bootstrap("wbthomason", "packer.nvim")
+bootstrap("Olical", "aniseed")
+bootstrap("lewis6991", "impatient.nvim")
 
 -- Loads plugins for Neovim with fennel.
 vim.g["aniseed#env"] = {
