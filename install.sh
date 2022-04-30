620f4d0b0df9564fe9d01df44323f492b556a44c
aktersnurra <grydholm@kth.se>
2022-04-30 18:22:18 +0200

    style: format install script

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-which nvim >/dev/null 2>&1 || (echo "Neovim not installed!" && exit 1)
-which luarocks >/dev/null 2>&1 || (echo "Luarocks not installed!" && exit 1)
-luarocks list | grep "fennel" >/dev/null 2>&1 || (echo "fennel is not installed" && exit 1)
-nvim --headless +qa && nvim --headless -c "autocmd User PackerComplete quitall"
+# Check dependencies
+which nvim >/dev/null 2>&1 ||
+(echo "Neovim not installed!" && exit 1)
+which luarocks >/dev/null 2>&1 ||
+(echo "Luarocks not installed!" && exit 1)
+luarocks list | grep "fennel" >/dev/null 2>&1 ||
+(echo "fennel is not installed" && exit 1)
+
+# Bootstrap installment
+nvim --headless +qa &&
+nvim --headless -c "autocmd User PackerComplete quitall"
