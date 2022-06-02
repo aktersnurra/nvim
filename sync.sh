48f944c4e6419118e7cf7ebb364bac508b1e1df9
Gustaf Rydholm <gustaf@nexure.io>
2022-06-02 18:56:14 +0200

    Rename install to sync

---
+++ sync.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+CONFIG=$HOME/.config/nvim
+rm -rf "$CONFIG/lua"
+
+# Check dependencies
+which nvim >/dev/null 2>&1 ||
+	(echo "Neovim not installed!" && exit 1)
+which luarocks >/dev/null 2>&1 ||
+	(echo "Luarocks not installed!" && exit 1)
+luarocks list | grep "fennel" >/dev/null 2>&1 ||
+	(echo "fennel is not installed" && exit 1)
+
+# Bootstrap installment
+(nvim --headless +qa >/dev/null 2>&1 &&
+	nvim --headless +"au User PackerComplete qa" +PackerSync +TSUpdateSync >/dev/null 2>&1 &&
+	echo "Successfully bootstrapped the config!") ||
+	(echo "Bootstrapping failed..." && exit 1)
