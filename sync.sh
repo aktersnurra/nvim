eb47c52b2460b1dc695ef4f855ab5483e9a12028
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-07-22 00:38:45 +0200

    Remove luarocks from sync

--- sync.sh
+++ sync.sh
@@ -6,9 +6,7 @@ rm -rf "$CONFIG/lua"
 # Check dependencies
 which nvim >/dev/null 2>&1 ||
 	(echo "Neovim not installed!" && exit 1)
-which luarocks >/dev/null 2>&1 ||
-	(echo "Luarocks not installed!" && exit 1)
-luarocks list | grep "fennel" >/dev/null 2>&1 ||
+which fennel >/dev/null 2>&1 ||
 	(echo "fennel is not installed" && exit 1)
 
 # Bootstrap installment
